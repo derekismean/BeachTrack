c6034d2af45d7d54eebdf35f528177bde15eb7ae
Derek Mean <116917344+derekismean@users.noreply.github.com>
2025-05-17 18:19:54 -0700

    Update db.js

--- beachtrack-derek/lib/config/db.js
+++ beachtrack-derek/lib/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const ConnectDB = async () => {
-    await mongoose.connect('mongodb+srv://DerekMean2:OP6B8lUsC71HOr53@beachtrack.z3sm6.mongodb.net/main?retryWrites=true&w=majority&appName=BeachTrack')
+    await mongoose.connect('mongodb+srv://user:password@beachtrack.z3sm6.mongodb.net/main?retryWrites=true&w=majority&appName=BeachTrack')
     console.log("DB Connected");
 }
